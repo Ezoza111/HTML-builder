d7d36ccc678ab96b225600044346d49a78bdb809
ezoza <ezozakhudayberdieva@mail.ru>
2024-01-22 16:17:11 +0500

    feat: add function 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const pathTextFile = path.join(__dirname, 'text.txt');
+
+const newText = fs.createWriteStream(pathTextFile);
+const { stdout, stdin } = require('process');
+
+stdout.write('Hi! Write your text:\n');
+stdin.on('data', (text) => {
+  if (text.toString().trim() === 'exit') {
+    stdout.write('You exit , Goodbye');
+    process.exit();
+  } else {
+    newText.write(text);
+  }
+});
+
+process.on('SIGINT', ()=> {
+    stdout.write('You exit , Goodbye');
+    process.exit()});
